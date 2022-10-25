7f689e1528f821e20d475a775621db1f38291d80
Matteo Tiberti <matteo.tiberti@gmail.com>
2022-10-25 12:37:32 +0200

    removed whitespace

--- mavisp/ingest.py
+++ mavisp/ingest.py
@@ -574,7 +574,7 @@ class MAVISFileSystem:
                 pmid_data = pmid_data.rename(columns={'PMID':'PMID / DOI'})
                 
                 this_df = this_df.join(pmid_data)
-            
+
             if 'ptm' in self._dir_list(self._tree[system][mode]):
 
                 ptm_dir = os.path.join(analysis_basepath, 'ptm')
