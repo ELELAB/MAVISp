9ada84d3e5ef419d9e0a5dd24fca78e3c6a73728
Matteo Tiberti <matteo.tiberti@gmail.com>
2023-03-17 14:39:26 +0100

    removed sas module from mavisp.py

--- mavisp/mavisp.py
+++ mavisp/mavisp.py
@@ -56,7 +56,7 @@ legend = f"""legend:
 {colored("    ~ module_name", 'yellow')}    warnings detected in this module, with no errors
     = module_name    this module is not available for this protein"""
 
-module_order = ['stability', 'local_interactions', 'cancermuts', 'references','sas', 'ptms', 'long_range', 'clinvar', 'alphafold', 'demask', 'gemme']
+module_order = ['stability', 'local_interactions', 'cancermuts', 'references', 'ptms', 'long_range', 'clinvar', 'alphafold', 'demask', 'gemme']
 
 def main():
 
