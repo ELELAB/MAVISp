4e00f034a1bcb696dc02894a74a71e505f161768
Matteo Tiberti <matteo.tiberti@gmail.com>
2023-11-17 16:29:35 +0100

    fixed spacing

--- streamlit_utils.py
+++ streamlit_utils.py
@@ -1,13 +1,13 @@
 # MAVISp - various utilities for MAVISp web server
-# Copyright (C) 2022 Matteo Tiberti, Danish Cancer Society
+# Copyright (C) 2022 Matteo Tiberti, Danish Cancer Society
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
@@ -153,5 +153,3 @@ class PTMCellRenderer {
     return this.eGui;
   }
 }''')
-
-
