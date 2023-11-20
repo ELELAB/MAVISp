ba1cae50b349fd733826a06deb7c688e6b1b3282
Matteo Tiberti <matteo.tiberti@gmail.com>
2023-11-20 16:32:14 +0100

    updated copyright and spacing

--- pages/3_Acknowledgements_and_data_usage.py
+++ pages/3_Acknowledgements_and_data_usage.py
@@ -1,13 +1,14 @@
 # MAVISp - Streamlit application
-# Copyright (C) 2022 Matteo Tiberti, Danish Cancer Society
+# Copyright (C) 2023 Matteo Tiberti, Danish Cancer Society
+#               2023 Elena Papaleo, Danish Cancer Society 
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
