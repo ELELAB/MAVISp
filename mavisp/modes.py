ab6f242b7afbd82b35cee05ad7423035dd2f9adc
Matteo Tiberti <matteo.tiberti@gmail.com>
2024-06-06 10:35:08 +0200

    added copyright infor for Eleni

--- mavisp/modes.py
+++ mavisp/modes.py
@@ -2,6 +2,7 @@
 # Copyright (C) 2022 Matteo Tiberti, Danish Cancer Society
 #           (C) 2023 Jérémy Vinhas, Danish Cancer Society
 #           (C) 2024 Pablo Sánchez-Izquierdo, Danish Cancer Society
+#           (C) 2024 Eleni Kiahaki, Danish Cancer Society
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
