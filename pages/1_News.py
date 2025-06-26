62806caf1666db838f39970241425c3e5525a3fd
Matteo Tiberti <matteo.tiberti@gmail.com>
2025-06-26 13:44:37 +0200

    modified news for workshop

--- pages/1_News.py
+++ pages/1_News.py
@@ -29,14 +29,14 @@ st.title("News and updates")
 
 st.header("In evidence")
 
+st.subheader("26/05/2025 - MAVISp BioCurator Training Workshop 2025 - deadline extended and programme available")
+st.badge("Training Event", icon="📖", color="green")
+st.text("""We have extended the deadline for joining the MAVISp BioCurator Training Workshop 2025 to July 4th. We have also made available a tentative programme for the workshop. For more information, see the Events page""")
+st.divider()
+
 st.subheader("15/05/2025 - MAVISp BioCurator Training Workshop 2025 announced")
 st.badge("Training Event", icon="📖", color="green")
 st.text("""We are organizing the first MAVISp BioCurator Training Workshop, to be held in early September 2025, fully online. For more information, see the Events page""")
-st.divider()
-
-st.subheader("26/05/2025 - MAVISp BioCurator Training Workshop 2025 - deadline extended")
-st.badge("Training Event", icon="📖", color="green")
-st.text("""We have extended the deadline for joining the MAVISp BioCurator Training Workshop 2025 to July 4th. For more information, see the Events page""")
 st.divider()
 
 st.header("Others")
